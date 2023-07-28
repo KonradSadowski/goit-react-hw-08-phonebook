ec6faf1d33e038be5a8e662b7ac6c96cce985df1
KonradSadowski <117109488+KonradSadowski@users.noreply.github.com>
2023-07-28 13:15:23 +0000

    Deploying to gh-pages from @ KonradSadowski/goit-react-hw-08-phonebook@b437ad123137533dc174e3f878145dc7fb7b4464 🚀

---
+++ static/js/11.cf148214.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[11],{11:function(e,t,r){r.r(t);var s=r(270),n=r(329);t.default=function(){return(0,n.jsxs)(n.Fragment,{children:[(0,n.jsx)(s.q,{children:(0,n.jsx)("title",{children:"Your tasks"})}),(0,n.jsx)("div",{children:"Tasks list"})]})}}}]);
+//# sourceMappingURL=11.cf148214.chunk.js.map
